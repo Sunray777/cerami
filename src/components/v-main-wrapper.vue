6c18a123c8d7c4090eeef8eb8475b59adeb2cbf8
Sunray777 <alpzm7@gmail.com>
2021-04-14 07:13:06 +0300

    Доделываю структуру

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="v-main-wrapper">
+<p>{{title}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'v-main-wrapper',
+components: {}, // дочернии компоненты нашего компонента
+props: {},
+data(){
+    return{
+        title: "Main wrapper"
+    }
+},
+computed: {}, // вычисления
+methods: {}, // анимации, показы модальных окон, нажатия кнопок итд
+watch: {}, // следят за изменениями компонента и что-то делают в случае этого изменения
+mounted() {
+    console.log('Hello, I am alive') // маунтед это то что выполняется когда компонент полностью загрузился
+}
+
+
+}
+
+</script>
+
+<style>
+
+.v-main-wrapper
+{
+display: flex;
+justify-content: center;
+align-items: center;
+max-width: 900px;
+margin: auto;
+}
+
+</style>
